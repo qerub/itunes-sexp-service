0d050b12faf8c75b1cc662982a146c092b0f93f9
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2013-04-26 22:21:47 +0200

    Don't use syntax quasiquotes unnecessarily

--- objc-plumbing.rkt
+++ objc-plumbing.rkt
@@ -19,12 +19,12 @@
 ; TODO: Refactor this
 (define-syntax-case tell-with-check ()
   ((_ #:type type target method-name rest ...)
-   #`(if (tell #:type _BOOL target respondsToSelector: #:type _SEL (selector method-name))
+   #'(if (tell #:type _BOOL target respondsToSelector: #:type _SEL (selector method-name))
          (objc->rkt-bridge (tell #:type type target method-name rest ...))
          (error '@ "No method ~a on ~a" 'method-name (objc->rkt-bridge (tell target description)))))
   ((_ rest ...)
-   #`(tell-with-check #:type _id rest ...)))
+   #'(tell-with-check #:type _id rest ...)))
 
 (define-syntax-case @ ()
   ((_ rest ...)
-   #`(tell-with-check rest ...)))
+   #'(tell-with-check rest ...)))
