3cd1b3471e782df6c4771fab4637453d0ef5f7e0
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2013-04-27 19:03:47 +0200

    Merge @/tell-with-check and tweak it

--- objc-plumbing.rkt
+++ objc-plumbing.rkt
@@ -17,14 +17,10 @@
       object))
 
 ; TODO: Refactor this
-(define-syntax-case tell-with-check ()
-  ((_ #:type type target method-name rest ...)
+(define-syntax-case @ ()
+  ([_ #:type type target method-name rest ...]
    #'(if (tell #:type _BOOL target respondsToSelector: #:type _SEL (selector method-name))
          (objc->rkt-bridge (tell #:type type target method-name rest ...))
          (error '@ "No method ~a on ~a" 'method-name (objc->rkt-bridge (tell target description)))))
   ((_ rest ...)
-   #'(tell-with-check #:type _id rest ...)))
-
-(define-syntax-case @ ()
-  ((_ rest ...)
-   #'(tell-with-check rest ...)))
+   #'[@ #:type _id rest ...]))
